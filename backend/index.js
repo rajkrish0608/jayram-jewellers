2a225c1fc30f530d0e156690cb63a25a8f1a87e1
Raj Krish <rajkrish0608@Rajs-MacBook-Air.local>
2025-11-30 22:27:20 +0530

    Update multiple files

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
     res.send('API is running...');
