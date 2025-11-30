e81e37f8341932d8255cd7d00914eea3db6fc353
Raj Krish <rajkrish0608@Rajs-MacBook-Air.local>
2025-11-30 23:05:41 +0530

    Feat: Enable Vercel Analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const playfair = Playfair_Display({
@@ -69,6 +70,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
         {children}
+        <Analytics />
       </body>
     </html>
   );
